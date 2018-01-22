99ad22e41656c663009f5578ff6cc05593d1374f
tschauner <munichboi@gmail.com>
2018-01-22 16:57:26 +0100

    - added uiviewcontroller extension / alert view

---
+++ JodelChallenge/Extensions/UIViewController+Utils.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewController+Utils.swift
+//  JodelChallenge
+//
+//  Created by Philipp Tschauner on 22.01.18.
+//  Copyright © 2018 Jodel. All rights reserved.
+//
+
+import Foundation
