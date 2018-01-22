d746e25ad21217f08a0a5d5d542902363f80c302
tschauner <munichboi@gmail.com>
2018-01-22 13:08:35 +0100

    - added realm manager

---
+++ JodelChallenge/Managers/RealmManager.swift
@@ -0,0 +1,14 @@
+//
+//  RealmManager.swift
+//  JodelChallenge
+//
+//  Created by Philipp Tschauner on 22.01.18.
+//  Copyright © 2018 Jodel. All rights reserved.
+//
+
+import Foundation
+
+class RealmManager {
+    
+    static let shared = RealmManager()
+}
