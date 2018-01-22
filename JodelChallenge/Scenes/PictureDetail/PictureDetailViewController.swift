c6dc36a2aa4a696abdb811a0506c0867de737996
tschauner <munichboi@gmail.com>
2018-01-22 21:25:47 +0100

    - added hero animation to picture detail vc

---
+++ JodelChallenge/Scenes/PictureDetail/PictureDetailViewController.swift
@@ -0,0 +1,108 @@
+//
+//  PictureDetailViewController.swift
+//  JodelChallenge
+//
+//  Created by Philipp Tschauner on 21.01.18.
+//  Copyright © 2018 Jodel. All rights reserved.
+//
+
+import UIKit
+import Hero
+import AlamofireImage
+
+extension PictureDetailViewController: PictureDetailProtocol {
+    
+    
+}
+
+func + (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x + right.x, y: left.y + right.y)
+}
+
+class PictureDetailViewController: UIViewController {
+    
+    @IBOutlet weak var pictureNameLabel: UILabel!
+    @IBOutlet weak var pictureImageView: UIImageView!
+    @IBOutlet weak var closeButton: UIButton!
+    
+    var pictureDetailViewModel = PictureDetailViewModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupButton()
+        setupViews()
+        initPanGesture()
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        animateButton()
+    }
+    
+    
+    func setupViews() {
+        
+        guard let photo = pictureDetailViewModel.flickrImage else { return }
+        
+        pictureNameLabel.heroID = "\(photo.id)_name"
+        pictureImageView.heroID = "\(photo.id)_name"
+        pictureImageView.af_setImage(withURL: photo.imageURL)
+        pictureNameLabel.text = photo.title
+        
+    }
+    
+    func initPanGesture() {
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(didPan))
+        pictureImageView.isUserInteractionEnabled = true
+        pictureImageView.addGestureRecognizer(pan)
+        pictureImageView.backgroundColor = .gray
+    }
+    
+    @objc func didPan(pan: UIPanGestureRecognizer) {
+        
+        let translation = pan.translation(in: nil)
+        let progress = translation.y / 2 / pictureImageView.frame.height
+        print(progress)
+        switch pan.state {
+        case .began:
+            // begin the transition as normal
+            dismiss(animated: true, completion: nil)
+        case .changed:
+            Hero.shared.update(progress)
+            
+            // update views' position (limited to only vertical scroll)
+            Hero.shared.apply(modifiers: [.position(CGPoint(x: pictureImageView.center.x, y:translation.y + pictureImageView.center.y))], to: pictureImageView)
+            Hero.shared.apply(modifiers: [.position(CGPoint(x: pictureNameLabel.center.x, y:translation.y + pictureNameLabel.center.y))], to: pictureNameLabel)
+            
+        default:
+            // end or cancel the transition based on the progress and user's touch velocity
+            if progress + pan.velocity(in: nil).y / view.bounds.height > 0.2 {
+                Hero.shared.finish()
+            } else {
+                Hero.shared.cancel()
+            }
+        }
+    }
+    
+    func animateButton() {
+        UIView.animateKeyframes(withDuration: 0.3, delay: 0, options: [], animations: {
+            self.closeButton.alpha = 1
+        }, completion: nil)
+    }
+    
+    
+    func setupButton() {
+        closeButton.alpha = 0
+        closeButton.addTarget(self, action: #selector(dismissView), for: .touchUpInside)
+    }
+    
+    @objc func dismissView() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+}
---
+++ JodelChallenge/Scenes/PictureDetail/PictureDetailViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  PictureDetailViewModel.swift
+//  JodelChallenge
+//
+//  Created by Philipp Tschauner on 22.01.18.
+//  Copyright © 2018 Jodel. All rights reserved.
+//
+
+import UIKit
+
+protocol PictureDetailProtocol: NSObjectProtocol {
+
+}
+
+class PictureDetailViewModel {
+
+    var delegate: PictureDetailProtocol?
+    
+    var flickrImage: FlickrPhoto? 
+
+    
+    
+}
